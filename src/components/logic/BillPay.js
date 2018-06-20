1996378a403841cdc2940d31528b75761f627d04
Zachary Haynie <zacharyrhaynie@gmail.com>
2018-06-20 09:22:15 -0600

    Add support for not round numbers

--- src/components/logic/BillPay.js
+++ src/components/logic/BillPay.js
@@ -12,21 +12,21 @@ export default class extends Component {
             event.preventDefault();
             alert(`You're tipping $${TipAmount()} which brings the total to $${BillAmount()}. This means each person pays $${AmountToPay()}`)
         };
-        let bill = parseInt(this.state.bill, 10);
+        let bill = parseFloat(this.state.bill, 10);
         let people = parseInt(this.state.people, 10);
         let tip = parseFloat((this.state.tip/100), 10);
 
 
         const TipAmount = () => {
-            return (bill * tip)
+            return parseFloat((bill * tip).toFixed(2))
         };
 
         const BillAmount = () => {
-            return (bill + TipAmount())
+            return parseFloat((bill + TipAmount()).toFixed(2))
         };
 
         const AmountToPay = () => {
-            return (BillAmount() / people)
+            return parseFloat((BillAmount() / people).toFixed(2))
         };
         return (
             <div>
@@ -34,17 +34,17 @@ export default class extends Component {
                     <h3>Welcome to Basic Split a Bill</h3>
                     <br/>
                     <h4>How much is the bill?</h4>
-                    <input type="number" placeholder="$20" required onChange={e => {
+                    <input type="number" min={0} step={0.01} placeholder="$20" required onChange={e => {
                         this.setState({bill: e.target.value})
                     }}/>
                     <br/>
                     <h4>How many people are splitting the bill?</h4>
-                    <input type="number" placeholder="2" required onChange={e => {
+                    <input type="number" min={0} placeholder="2" required onChange={e => {
                         this.setState({people: e.target.value})
                     }}/>
                     <br/>
                     <h4>What percent tip are you giving?</h4>
-                    <input type="number" placeholder="20%" required onChange={e => {
+                    <input type="number" min={0} placeholder="20%" required onChange={e => {
                         this.setState({tip: e.target.value})
                     }}/>
                     <br/>
